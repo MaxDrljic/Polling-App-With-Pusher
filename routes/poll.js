6a7a0cd1558acc399af26e11f6e8782a9cb13e54
MaxDrljic <max_spree@hotmail.com>
2018-02-01 10:00:04 +0100

    Initalized Pusher & UI

--- routes/poll.js
+++ routes/poll.js
@@ -1,8 +1,27 @@
 const express = require('express');
 const router = express.Router();
 
+const Pusher = require('pusher');
+
+var pusher = new Pusher({
+  appId: '467276',
+  key: '1d832592855516476586',
+  secret: 'a7b23c894c9477561051',
+  cluster: 'eu',
+  encrypted: true
+});
+
 router.get('/', (req, res) => {
   res.send('Poll');
+});
+
+router.post('/', (req, res) => {
+  pusher.trigger('os-poll', 'os-vote', {
+    points: 1,
+    os: req.body.os
+  });
+
+  return res.json({success: true, message: 'Thank you for voting!'});
 });
 
 module.exports = router;
